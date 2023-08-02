bdadb80bb3de8e37c00f10c6acf4312c6a93f173
Matheus de Oliveira Marques <theu1421@gmail.com>
2023-08-02 14:32:26 -0300

    did a validation for data base

--- src/DataBaser.py
+++ src/DataBaser.py
@@ -4,14 +4,15 @@ conn = sqlite3.connect('UserData.db')
 
 cursor = conn.cursor()
 
-cursor.execute(""" 
-CREATE TABLE IF NOT EXISTS Users(
-    Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    Name TEXT NOT NULL,
-    Email TEXT NOT NULL,
-    User TEXT NOT NULL,
-    Password TEXT NOT NULL
-);
-""")
-
-print ("Conectado com o banco de Dados")
+if cursor != '':
+    cursor.execute(""" 
+    CREATE TABLE IF NOT EXISTS Users(
+        Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        Name TEXT NOT NULL,
+        Email TEXT NOT NULL,
+        User TEXT NOT NULL,
+        Password TEXT NOT NULL
+    );
+    """)
+    
+    print ("Conectado com o banco de Dados")
